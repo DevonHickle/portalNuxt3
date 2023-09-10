4dd00a10deac8df01be6e8161a2f205e184bb3d4
devonhickle <devonhickle@yahoo.com>
2023-09-10 08:05:18 -0500

    adding vuetify

--- devportal/nuxt.config.ts
+++ devportal/nuxt.config.ts
@@ -23,5 +23,16 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase"],
-  css: ["@/assets/css/main.css"],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.$env.DEBUG": false,
+    },
+  },
 });
